4df70588cbfb69b549f5bf2a3dc4ce9193dbf94d
Ahmed Safdar <asafdar1138@gmail.com>
2019-01-04 20:52:39 -0600

    init

---
+++ FlowerShop/Controllers/FlowersController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowerShop.Controllers
+{
+    public class FlowersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
